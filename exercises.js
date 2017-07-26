51ced9e6b61f93177400dad0273559ad00469ae0
phytertek <ryan.phytertek@gmail.com>
2017-07-26 02:13:04 -0400

    problem 3 passing

--- exercises.js
+++ exercises.js
@@ -16,6 +16,7 @@ function areSameLength(str1, str2) {
     //return true if the two strings have the same length
     //otherwise return false
     //code here
+    return str1.length === str2.length;
 }
 
 function areEqual(x, y) {
